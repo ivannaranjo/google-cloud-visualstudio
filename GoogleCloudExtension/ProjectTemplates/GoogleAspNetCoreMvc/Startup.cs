3cd1c1cfcdd3f371898809d1391ffa0d1341bb22
GOOGLE\talarico <talarico@google.com>
2017-02-27 13:17:30 -0800

    Fix bad copy and update template

--- GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreMvc/Startup.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreMvc/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace GoogleAspNetCoreMvc3
+namespace $safeprojectname$
 {
     public class Startup
     {
