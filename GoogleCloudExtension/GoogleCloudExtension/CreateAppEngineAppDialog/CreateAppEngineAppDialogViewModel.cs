e8303f34f3eae42e997261962d6a39ef17a9087e
Ivan Naranjo <ivann@google.com>
2017-04-28 16:22:22 +0100

    Adding initial drop of the dialog files.

---
+++ GoogleCloudExtension/GoogleCloudExtension/CreateAppEngineAppDialog/CreateAppEngineAppDialogViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GoogleCloudExtension.Theming;
+using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.CreateAppEngineAppDialog
+{
+    class CreateAppEngineAppDialogViewModel : ViewModelBase
+    {
+        private static readonly string[] s_regions = new string[]
+        {
+            "us-central",
+            "europe-west",
+            "us-east1",
+            "asia-northeast1"
+        };
+
+        private readonly CreateAppEngineAppDialogWindow _owner;
+        private string _selectedRegion;
+
+        public IEnumerable<string> Regions => s_regions;
+
+        public string SelectedRegion
+        {
+            get { return _selectedRegion; }
+            set { SetValueAndRaise(ref _selectedRegion, value); }
+        }
+
+        public bool AppCreated { get; private set; }
+
+        public ProtectedCommand CreateAppCommand { get; }
+
+        public CreateAppEngineAppDialogViewModel(CreateAppEngineAppDialogWindow owner)
+        {
+            _owner = owner;
+
+            CreateAppCommand = new ProtectedCommand(OnCreateAppCommand);
+        }
+
+        private void OnCreateAppCommand()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GoogleCloudExtension/GoogleCloudExtension/CreateAppEngineAppDialog/CreateAppEngineAppDialogWindow.cs
@@ -0,0 +1,29 @@
+﻿using GoogleCloudExtension.Theming;
+using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.CreateAppEngineAppDialog
+{
+    class CreateAppEngineAppDialogWindow : CommonDialogWindowBase
+    {
+        public CreateAppEngineAppDialogViewModel ViewModel { get; }
+
+        private CreateAppEngineAppDialogWindow():
+            base("Create App Engine App")
+        {
+            ViewModel = new CreateAppEngineAppDialogViewModel(this);
+            Content = new CreateAppEngineAppDialogContent { DataContext = ViewModel };
+        }
+
+        public static bool PromptUser()
+        {
+            var dialog = new CreateAppEngineAppDialogWindow();
+            dialog.ShowModal();
+            return dialog.ViewModel.AppCreated;
+        }
+    }
+}
