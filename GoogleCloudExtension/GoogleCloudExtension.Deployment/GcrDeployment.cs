30354d58a5afee680d8d6d95b873bab0a1fffd20
Ivan Naranjo <ivann@google.com>
2017-09-12 16:08:48 +0100

    Adding deployment to GCR.

---
+++ GoogleCloudExtension/GoogleCloudExtension.Deployment/GcrDeployment.cs
@@ -0,0 +1,69 @@
+﻿using GoogleCloudExtension.GCloud;
+using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.Deployment
+{
+    public static class GcrDeployment
+    {
+        public class DeploymentOptions
+        {
+            public string ImageName { get; set; }
+
+            public string ImageTag { get; set; }
+
+            public GCloudContext GCloudContext { get; set; }
+        }
+
+        public static async Task<bool> PublishProjectAsync(
+            IParsedProject project,
+            DeploymentOptions options,
+            IProgress<double> progress,
+            IToolsPathProvider toolsPathProvider,
+            Action<string> outputAction)
+        {
+            var stageDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(stageDirectory);
+            progress.Report(0.1);
+
+            using (var cleanup = new Disposable(() => CommonUtils.Cleanup(stageDirectory)))
+            {
+                var appRootPath = Path.Combine(stageDirectory, "app");
+                var buildFilePath = Path.Combine(stageDirectory, "cloudbuild.yaml");
+
+                if (!await ProgressHelper.UpdateProgress(
+                        NetCoreAppUtils.CreateAppBundleAsync(project, appRootPath, toolsPathProvider, outputAction),
+                        progress,
+                        from: 0.1, to: 0.3))
+                {
+                    Debug.WriteLine("Failed to create app bundle.");
+                    return false;
+                }
+
+                NetCoreAppUtils.CopyOrCreateDockerfile(project, appRootPath);
+                var image = CloudBuilderUtils.CreateBuildFile(
+                    project: options.GCloudContext.ProjectId,
+                    imageName: options.ImageName,
+                    imageVersion: options.ImageTag,
+                    buildFilePath: buildFilePath);
+
+                if (!await ProgressHelper.UpdateProgress(
+                    GCloudWrapper.BuildContainerAsync(buildFilePath, appRootPath, outputAction, options.GCloudContext),
+                    progress,
+                    from: 0.4, to: 1.0))
+                {
+                    Debug.WriteLine("Failed to build image.");
+                    return false;
+                }
+                progress.Report(1.0);
+                return true;
+            }
+        }
+    }
+}
--- GoogleCloudExtension/GoogleCloudExtension/PublishDialogSteps/GcrStep/GcrStepViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/PublishDialogSteps/GcrStep/GcrStepViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using GoogleCloudExtension.Accounts;
+using GoogleCloudExtension.Deployment;
+using GoogleCloudExtension.GCloud;
 using GoogleCloudExtension.PublishDialog;
 using GoogleCloudExtension.Utils;
+using GoogleCloudExtension.VsVersion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,7 @@ namespace GoogleCloudExtension.PublishDialogSteps.GcrStep
     class GcrStepViewModel : PublishDialogStepBase
     {
         private readonly GcrStepContent _content;
-        private IPublishDialog _dialog;
+        private IPublishDialog _publishDialog;
         private string _projectName;
         private string _imageTag;
         private string _imageName;
@@ -43,6 +46,8 @@ namespace GoogleCloudExtension.PublishDialogSteps.GcrStep
         private GcrStepViewModel(GcrStepContent content)
         {
             _content = content;
+
+            CanPublish = true;
         }
 
         #region Overrides for PublishDialogStepBase
@@ -51,11 +56,72 @@ namespace GoogleCloudExtension.PublishDialogSteps.GcrStep
 
         public override void OnPushedToDialog(IPublishDialog dialog)
         {
-            _dialog = dialog;
+            _publishDialog = dialog;
 
             _projectName = CredentialsStore.Default.CurrentProjectId;
             ImageName = dialog.Project.Name.ToLower();
             ImageTag = GcpPublishStepsUtils.GetDefaultVersion();
+        }
+
+        public override async void Publish()
+        {
+            try
+            {
+                ShellUtils.SaveAllFiles();
+
+                var project = _publishDialog.Project;
+
+                var gcloudContext = new GCloudContext
+                {
+                    CredentialsPath = CredentialsStore.Default.CurrentAccountPath,
+                    ProjectId = CredentialsStore.Default.CurrentProjectId,
+                    AppName = GoogleCloudExtensionPackage.ApplicationName,
+                    AppVersion = GoogleCloudExtensionPackage.ApplicationVersion,
+                };
+
+                var options = new GcrDeployment.DeploymentOptions
+                {
+                    ImageName = ImageName,
+                    ImageTag = ImageTag,
+                    GCloudContext = gcloudContext
+                };
+
+                GcpOutputWindow.Activate();
+                GcpOutputWindow.Clear();
+                GcpOutputWindow.OutputLine($"Stating build of image {ImageFullName}");
+
+                _publishDialog.FinishFlow();
+
+                bool result;
+                using (StatusbarHelper.Freeze())
+                using (StatusbarHelper.ShowDeployAnimation())
+                using (var progress = StatusbarHelper.ShowProgressBar("Building Docker image"))
+                using (ShellUtils.SetShellUIBusy())
+                {
+                    result = await GcrDeployment.PublishProjectAsync(
+                        project,
+                        options,
+                        progress,
+                        VsVersionUtils.ToolsPathProvider,
+                        GcpOutputWindow.OutputLine);
+                }
+
+                if (result)
+                {
+                    StatusbarHelper.SetText("Docker image build succeeded");
+                }
+                else
+                {
+                    GcpOutputWindow.OutputLine($"Failed to build image {ImageFullName}.");
+                    StatusbarHelper.SetText("Docker image build failed");
+                }
+            }
+            catch (Exception ex) when (!ErrorHandlerUtils.IsCriticalException(ex))
+            {
+                GcpOutputWindow.OutputLine($"Failed to build image {ImageFullName}");
+                StatusbarHelper.SetText(Resources.PublishFailureStatusMessage);
+                _publishDialog.FinishFlow();
+            }
         }
 
         #endregion
